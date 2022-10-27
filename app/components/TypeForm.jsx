f7eb7c4fe5d19e6fe651147f127a4db3f45f5034
Andrea Altamirano <andrea.altamirano3677@hotmail.com>
2022-10-27 17:36:46 +0200

    remove synchronous script from type form component to avoid build error in vercel

--- app/components/TypeForm.jsx
+++ app/components/TypeForm.jsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 
 function TypeForm() {
     return <>
-        <Head>
+        {/* <Head>
             <script src="//embed.typeform.com/next/embed.js"></script>
-        </Head>
+        </Head> */}
 
         <div data-tf-widget="snHc9aHi" data-tf-opacity="100" style={{ width: '100%', height: '100%' }}></div>
     </>
